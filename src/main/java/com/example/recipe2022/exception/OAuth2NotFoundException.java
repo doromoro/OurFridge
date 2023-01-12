d8fa8f0cdae36dbedda174b8a5887f383eaa2c09
BurgerC <66655923+PartyForYou@users.noreply.github.com>
2023-01-12 16:24:42 +0900

    Delete src/main/java/com/example/recipe2022 directory
    k

--- src/main/java/com/example/recipe2022/exception/OAuth2NotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.example.recipe2022.exception;
-
-public class OAuth2NotFoundException extends RuntimeException {
-    public OAuth2NotFoundException(String message) {
-        super(message);
-    }
-}
